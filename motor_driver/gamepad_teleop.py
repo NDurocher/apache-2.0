52bf786809b770c3c192f0ee407e35e1eb48de6a
Nate <nate@localhostjet.localdomain>
2025-10-09 10:18:08 -0600

    Add gamepad control and update motor control for proper diff drive motor commands

---
+++ motor_driver/gamepad_teleop.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import rospy
+from geometry_msgs.msg import Twist
+import pygame
+import sys
+
+class GamepadTeleop:
+    def __init__(self):
+        rospy.init_node('gamepad_teleop')
+        self.pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
+        self.rate = rospy.Rate(20)  # 20 Hz for smoother control
+        
+        # Movement parameters
+        self.max_linear_speed = 1.0
+        self.max_angular_speed = 1.5
+        
+        # Deadzone for analog sticks (prevent drift)
+        self.deadzone = 0.15
+        
+        # Initialize pygame
+        pygame.init()
+        pygame.joystick.init()
+        
+        # Check for connected controllers
+        if pygame.joystick.get_count() == 0:
+            rospy.logerr("No game controller found!")
+            sys.exit(1)
+        
+        # Use first controller
+        self.joystick = pygame.joystick.Joystick(0)
+        self.joystick.init()
+        
+        rospy.loginfo("Connected to: {}".format(self.joystick.get_name()))
+        rospy.loginfo("Number of axes: {}".format(self.joystick.get_numaxes()))
+        rospy.loginfo("Number of buttons: {}".format(self.joystick.get_numbuttons()))
+        
+    def apply_deadzone(self, value):
+        """Apply deadzone to analog stick values"""
+        if abs(value) < self.deadzone:
+            return 0.0
+        # Scale the value so deadzone maps to 0 and 1.0 stays at 1.0
+        return (abs(value) - self.deadzone) / (1.0 - self.deadzone) * (1.0 if value > 0 else -1.0)
+    
+    def run(self):
+        rospy.loginfo("Gamepad teleop started.")
+        rospy.loginfo("Left stick: forward/back, Right stick: turn")
+        rospy.loginfo("Press button 0 (usually A/X) to quit")
+        
+        while not rospy.is_shutdown():
+            pygame.event.pump()  # Process event queue
+            
+            twist = Twist()
+            
+            # Read analog sticks
+            # Axis mapping varies by controller, these are common:
+            # Axis 0: Left stick horizontal
+            # Axis 1: Left stick vertical  
+            # Axis 2: Right stick horizontal
+            # Axis 3: Right stick vertical
+            
+            # Left stick Y-axis for forward/backward (inverted because down is positive)
+            if self.joystick.get_numaxes() >= 2:
+                left_y = -self.joystick.get_axis(1)
+                twist.linear.x = self.apply_deadzone(left_y) * self.max_linear_speed
+            
+            # Right stick X-axis for turning
+            if self.joystick.get_numaxes() >= 3:
+                right_x = self.joystick.get_axis(2)
+                twist.angular.z = self.apply_deadzone(right_x) * self.max_angular_speed
+            
+            # Check for quit button (button 0, usually A on Xbox or X on PlayStation)
+            if self.joystick.get_numbuttons() > 0:
+                if self.joystick.get_button(0):
+                    rospy.loginfo("Quit button pressed")
+                    break
+            
+            self.pub.publish(twist)
+            self.rate.sleep()
+        
+        pygame.quit()
+        rospy.loginfo("Gamepad teleop stopped.")
+
+if __name__ == '__main__':
+    try:
+        teleop = GamepadTeleop()
+        teleop.run()
+    except rospy.ROSInterruptException:
+        pass
--- motor_driver/motor_control.py
+++ motor_driver/motor_control.py
@@ -8,28 +8,28 @@ import numpy as np
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 
-motor_1_channels = [40, 38]  # in1, in2
-motor_2_channels = [19, 21]  # in3, in4
+right_motor_channels = [38, 40]  # in1, in2
+left_motor_channels = [19, 21]  # in3, in4
 
-GPIO.setup(motor_1_channels, GPIO.OUT)
-GPIO.setup(motor_2_channels, GPIO.OUT)
+GPIO.setup(right_motor_channels, GPIO.OUT)
+GPIO.setup(left_motor_channels, GPIO.OUT)
 
 class GPIOController:
     def __init__(self):
         rospy.init_node('gpio_controller', anonymous=True)
-        rospy.Subscriber('/cmd_vel', Twist, self.update_GPIO)
+        rospy.Subscriber('/cmd_vel', Twist, self.handle_twist_msg)
 
-        self.linear_cmd = 0 # [-1,0,1]
-        self.angular_cmd = 0 # [-pi/2,pi/2]
-        motor_pwm_channel = 33
-        GPIO.setup(motor_pwm_channel, GPIO.OUT, initial=GPIO.HIGH)
-        # self.p = GPIO.PWM(motor_pwm_channel, 50)
-        # self.val = 10
-        # self.incr = 10
-        # self.p.start(self.val)
+        # GPIO setup
+        #GPIO.setmode(GPIO.BOARD)
+        #GPIO.setwarnings(False)
 
-    # def __del__(self):
-        # self.p.stop()
+        #right_motor_channels = [38, 40]  # in1, in2
+        #left_motor_channels = [19, 21]  # in3, in4
+
+        #GPIO.setup(self.right_motor_channels, GPIO.OUT)
+        #GPIO.setup(self.left_motor_channels, GPIO.OUT)
+
+        rospy.loginfo("GPIO setup")
 
     def update_GPIO(self, msg):
         if msg.angular.z == 0:
@@ -60,53 +60,27 @@ class GPIOController:
             GPIO.output(38, GPIO.LOW)
             GPIO.output(21, GPIO.LOW)
 
-    def update_command(self, incoming_cmd):
-        self.linear_cmd = incoming_cmd["linear"]
-        self.angular_cmd = incoming_cmd["angular"]
-        # self.linear_cmd = np.clip(self.linear_cmd + incoming_cmd["linear"], -1, 1)
-        # self.angular_cmd = np.clip(self.angular_cmd + incoming_cmd["angular"], -np.pi/2, np.pi/2)
+    def handle_twist_msg(self, msg):
+        left_cmd = np.clip(msg.linear.x + msg.angular.z/2, -1, 1)
+        right_cmd = np.clip(msg.linear.x - msg.angular.z/2, -1,1)
+        self.update_motor(left_cmd, left_motor_channels)
+        self.update_motor(right_cmd, right_motor_channels)
 
-    #def handle_key_input(self, msg):
-        #key = msg.data
-
-        #new_cmd = {'linear':0, 'angular':0}
-        #if key == 'right':
-        #    print("Right arrow pressed!")
-        #    new_cmd['linear'] = 0
-        #    new_cmd['angular'] = -np.pi/2
-        #elif key == 'left':
-        #    print("Left arrow pressed!")
-        #    new_cmd['linear'] = 0
-        #    new_cmd['angular'] = np.pi/2
-        #elif key == 'up':
-        #    print("Up arrow pressed!")
-        #    new_cmd['linear'] = 1
-        #    new_cmd['angular'] = 0
-        #elif key == 'down':
-        #    print("Down arrow pressed!")
-        #    new_cmd['linear'] = -1
-        #    new_cmd['angular'] = 0
-        #elif key == 'space':
-        #    print("STOP!!!!")
-        #    new_cmd['linear'] = 0
-        #    new_cmd['angular'] = 0
-        #
-        #self.update_command(new_cmd)
-        #self.update_GPIO(msg)
-        # elif key == "w":
-        #     if (self.val + self.incr) <= 100:
-        #         self.val += self.incr
-        #         self.p.ChangeDutyCycle(self.val)
-        #         print(f"speed: {self.val}")
-        # elif key == "s":
-        #     if (self.val - self.incr) >= 0:
-        #         self.val -= self.incr
-        #         self.p.ChangeDutyCycle(self.val)
-        #         print(f"speed: {self.val}")
+    def update_motor(self, cmd, motor_channels):
+        if cmd > 0.0:
+          GPIO.output(motor_channels[0], GPIO.HIGH)
+          GPIO.output(motor_channels[1], GPIO.LOW)
+        elif cmd < 0.0:
+          GPIO.output(motor_channels[0], GPIO.LOW)
+          GPIO.output(motor_channels[1], GPIO.HIGH)
+        else:
+          GPIO.output(motor_channels[0], GPIO.LOW)
+          GPIO.output(motor_channels[1], GPIO.LOW)
 
 if __name__ == "__main__":
     gpio_controller = GPIOController()
     try:
+        rospy.loginfo("Running")
         rospy.spin()
     except rospy.ROSInterruptException:
         GPIO.cleanup()
